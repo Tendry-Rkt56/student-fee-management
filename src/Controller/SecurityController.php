a2eb50057d5bdbcb0221954e68b8aeb0a661ce6e
Tendry <tendryzephyrin@gmail.com>
2025-01-17 19:19:38 +0100

    Création des entités et controller liées aux utilisateurs

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+
+class SecurityController extends Controller
+{
+
+
+     public function loginView()
+     {
+          return $this->render('security.login');
+     }
+
+     public function login(array $data = [])
+     {
+          $login = $this->getManager(User::class)->login($data);
+          try {
+               if ($login) return $this->redirect('app.dashboard');
+               throw new \Exception("Erreur serveur");
+          }
+          catch(\Exception $e) {
+               $_SESSION['danger'] = $e->getMessage();
+               return $this->redirect('app.login');
+          }
+     }
+
+}
