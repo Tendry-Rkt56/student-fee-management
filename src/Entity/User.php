c401de600269eca543f0f5de444b0e459f17a50e
Tendry <tendryzephyrin@gmail.com>
2025-01-20 08:36:00 +0100

    Creation du register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -25,4 +25,18 @@ class User extends Entity
           return false;
      }
 
+     public function register(array $data = [], array $files = [])
+     {
+          $sql = "INSERT INTO users(nom, prenom, email, image, password) VALUES (:nom, :prenom, :email, :image, :password)";
+          $query = $this->db->getConn()->prepare($sql);
+          extract($data);
+          $query->bindValue(':nom', $nom, \PDO::PARAM_STR);
+          $query->bindValue(':prenom', $prenom, \PDO::PARAM_STR);
+          $query->bindValue(':email', Validator::unique('users', 'email', 'email', $email), \PDO::PARAM_STR);
+          $query->bindValue(':image', imageUpload($files['image'], "images/users/"));
+          $query->bindValue(':password', password_hash($password, PASSWORD_DEFAULT), \PDO::PARAM_STR);
+          $_SESSION['success'] = "Nouvel utilisateur crée";
+          return $query->execute();
+     }
+
 }
---
+++ templates/security/register.html.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+     <html lang="fr">
+     <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Nouvel utilisateur</title>
+          <?php require_once 'components/head.html' ?>
+          <script src="/assets/script/nameFormatter.js" defer></script>
+     </head>
+     <body>
+
+          <?php require_once 'components/base.html.php' ?>
+
+          <div style="box-shadow:-4px 4px 10px 0Px rgba(0,0,0,0.4);padding:30px;position:absolute;top:20%;left:220px;width:calc(100% - 200px)">
+               <div class="container-fluid d-flex align-items-center justify-content-center flex-column gap-3">
+                    <h2 class="align-self-start">Nouveau</h2>
+                    <?php if (isset($_SESSION)): ?>
+                         <?php foreach($_SESSION as $key => $value): ?>
+                              <?php if ($key == 'danger' || $key == 'success'):?>
+                                   <p class="d-flex align-items-center justify-content-center container-sm alert alert-<?=$key?>"><?=$value?></p>
+                                   <?php unset($_SESSION[$key])?>
+                              <?php endif?>
+                         <?php endforeach?>
+                    <?php endif ?>
+                    <form method="POST" action="" enctype="multipart/form-data" class="justify-self-start align-self-start container d-flex align-items-center justify-content-start flex-column gap-3">
+                         <div class="row container">
+                              <div class="col-sm-6 d-flex align-items-center justify-content-center flex-row gap-1">
+                                   <label style="width:20%" for="nom" class="fw-bolder">Nom:</label>
+                                   <input id="name" style="width:80%" type="text" name="nom" id="nom" class="form-control" placeholder="Nom...">
+                              </div>
+                              <div class="col-sm-6 d-flex align-items-center justify-content-center flex-row gap-1">
+                                   <label style="width:20%" for="prenom" class="fw-bolder">Prénom:</label>
+                                   <input id="prenom" style="width:80%" type="text" name="prenom" id="prenom" class="form-control" placeholder="Prénom...">
+                              </div>
+                         </div>
+                         <div class="row container">
+                              <div class="col-sm-6 d-flex align-items-center justify-content-center flex-row gap-1">
+                                   <label style="width:20%" for="email" class="fw-bolder">Email:</label>
+                                   <input type="text" style="width:80%;" name="email" placeholder="Email..." class="form-control" id="email">
+                              </div>
+                              <div class="col-sm-6 d-flex align-items-center justify-content-center flex-row gap-1">
+                                   <label style="width:20%" for="image" class="fw-bolder">Mot de passe:</label>
+                                   <input type="text" name="password" class="form-control" style="width:80%" placeholder="Mot de passe">
+                              </div>
+                         </div>
+                         <div class="row container">
+                              <div class="col-sm-6 d-flex align-items-center justify-content-center flex-row gap-1">
+                                   <label style="width:20%" for="email" class="fw-bolder">Image:</label>
+                                   <input type="file" style="width:80%;" name="image" placeholder="Image associée..." class="form-control" id="image">
+                              </div>
+                         </div>
+                         <input type="submit" class="my-3 btn btn-primary" value="Enregistrer">
+                    </form>
+               </div>
+          </div>
+
+     </body>
+     </html>
